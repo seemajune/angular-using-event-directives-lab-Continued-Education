adf3c54c74939160944bdb969fb00d51672a1ff3
seemajune <seemashariat@gmail.com>
2016-03-03 14:09:43 -0500

    finished

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,25 @@
 function VoteController() {
-    this.votes = 0;
+  var vm = this;
+    vm.votes = 0;
+    vm.hasVoted = false;
+    vm.hasRemovedVote = false;
 
     this.incrementVotes = function () {
-
+      if(!vm.hasVoted){
+        vm.votes++;
+        vm.hasVoted = true;
+      }
+      else if (vm.hasVoted && vm.hasRemovedVote){
+        vm.votes++;
+        vm.hasRemovedVote = false;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (vm.votes && vm.hasVoted && !vm.hasRemovedVote){
+        vm.votes--;
+        vm.hasRemovedVote = true;
+      }
     };
 }
 
